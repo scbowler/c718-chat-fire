0b8b0e74e01e059b59e928003284d30474298e15
Scott Bowler <scott.bowler@learningfuze.com>
2018-10-23 14:54:10 -0700

    Room creation and initial room view working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import types from './types';
 import { db } from '../firebase';
 
-export const getMessages = () => dispatch => {
-    const dbRef = db.ref('/chat-messages');
+export const getMessages = (chatId) => dispatch => {
+    const dbRef = db.ref(`/chat-logs/${chatId}`);
 
     dbRef.on('value', (snapshot) => {
         console.log('DB Snapshot:', snapshot.val());
@@ -14,4 +14,35 @@ export const getMessages = () => dispatch => {
     });
 
     return dbRef;
+}
+
+export const getRoomInfo = roomId => dispatch => {
+    const dbRef = db.ref(`/chat-rooms/${roomId}`);
+
+    dbRef.on('value', snapshot => {
+        console.log('Room Snapshot:', snapshot.val());
+
+        dispatch({
+            type: types.GET_ROOM_INFORMATION,
+            roomInfo: snapshot.val()
+        });
+    });
+
+    return dbRef;
+}
+
+export const createChatRoom = roomDetails => async dispatch => {
+    const botMessage = {
+        message: `Welcome to ${roomDetails.title}`,
+        name: 'Chat-Bot'
+    };
+    
+    const logKey = db.ref('/chat-logs').push().key;
+
+    roomDetails.chatId = logKey;
+    const roomRef = await db.ref('/chat-rooms').push(roomDetails);
+
+    await db.ref(`/chat-logs/${logKey}`).push(botMessage);
+
+    return roomRef.key;
 }
---
+++ src/components/rooms/chat.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getMessages, getRoomInfo } from '../../actions';
+
+class Chat extends Component {
+    roomRef = null;
+    chatRef = null;
+
+    componentDidMount(){
+        const { getRoomInfo, match: { params } } = this.props;
+        
+        this.roomRef = getRoomInfo(params.room_id);
+    }
+
+    componentDidUpdate(prevProps){
+        const { chatId, getMessages } = this.props;
+        
+        if(chatId && prevProps.chatId !== chatId){
+            this.chatRef = getMessages(chatId);
+        }
+    }
+
+    componentWillUnmount(){
+        if(this.roomRef){
+            this.roomRef.off();
+        }
+        if(this.chatRef){
+            this.chatRef.off();
+        }
+    }
+
+    render(){
+        console.log('Chat Props:', this.props);
+        const { description, messages, title, topic } = this.props;
+        const messageElements = Object.keys(messages).map(key => {
+            const {name, message} = messages[key];
+            return (
+                <li key={key} className="collection-item">
+                    <b>{name}:</b> {message}
+                </li>
+            );
+        });
+        return (
+            <div>
+                <div className="center">
+                    <h1>{title || 'Chat Room'}</h1>
+                    <h5 className="grey-text">{topic}</h5>
+                    <p className="grey-text">{description}</p>
+                </div>
+                
+                <ul className="collection">
+                    {messageElements}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return { ...state.chat };
+}
+
+export default connect(mapStateToProps, {
+    getMessages,
+    getRoomInfo
+})(Chat);
